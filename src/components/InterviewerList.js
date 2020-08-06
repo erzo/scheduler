bbcde82d1b27ea61f14a5422713dc5716ea26970
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-08-05 17:02:35 -0700

    modified

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -14,8 +14,9 @@ export default function InterviewerList(props) {
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        selected={interviewer.id === props.interviewer}
+        selected={props.interviewer === interviewer.id ? true : false}
         setInterviewer={props.setInterviewer}
+        // setInterviewer={props.onChange}
         />
       ))
     // console.log(interviewerList);
