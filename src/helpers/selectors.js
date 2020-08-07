3e33bddc2d5e617017a81e786ffd6868850da191
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-08-06 21:18:26 -0700

    added a new function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,4 @@
-export default function getAppointmentsForDay(state, day) {
+const getAppointmentsForDay = function(state, day) {
   let days = state.days;
   let appointments = state.appointments;
   let selectedDay = [];
@@ -15,3 +15,22 @@ export default function getAppointmentsForDay(state, day) {
   console.log(appointment);
   return appointment;
 };
+
+const getInterview = function(state, interview){
+  if(!interview){
+    return null;
+  } else{
+    let interviewer = interview.interviewer;
+    let student = interview.student;
+    for(let inter in state.interviewers){
+      if(interviewer === Number(inter)){
+        return {student:student, interviewer:state.interviewers[inter]};
+      }
+    }
+  }
+}
+
+export {
+  getAppointmentsForDay, 
+  getInterview
+}
