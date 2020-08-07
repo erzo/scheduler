0b46af1397a2ed786948865e3790543c54609738
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-08-06 19:38:25 -0700

    set up a function that gets appointments for the specific day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,17 @@
+export default function getAppointmentsForDay(state, day) {
+  let days = state.days;
+  let appointments = state.appointments;
+  let selectedDay = [];
+  let appointment = [];
+  for (let i = 0; i < days.length; i++) {
+    if (days[i].name === day) {
+      selectedDay = (days[i].appointments);
+    }
+  }
+  for (let j = 0; j < selectedDay.length; j++) {
+
+    appointment.push(appointments[selectedDay[j]]);
+  }
+  console.log(appointment);
+  return appointment;
+};
