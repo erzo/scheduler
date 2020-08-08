f9bad37a27cdc17fccd1a5ece40e70b643a59cd5
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-08-07 21:46:57 -0700

    create the delete function

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,6 +5,7 @@ import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
 import Status from "./Status";
+import Confirm from "./Confirm";
 
 
 import useVisualMode from "../../hooks/useVisualMode";
@@ -15,6 +16,8 @@ const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
+const DELETE = "DELETE";
+const CONFIRM = "CONFIRM";
 
 export default function Appointment(props) {
   
@@ -38,6 +41,17 @@ export default function Appointment(props) {
       console.log(err);
     })
   }
+
+  function deleteAction() {
+    transition(DELETE, true);
+    props.cancelInterview(props.id)
+    .then(()=>{
+      transition(EMPTY);
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  }
   
   return(
     <article className="appointment">
@@ -47,6 +61,8 @@ export default function Appointment(props) {
         <Show
         student={props.interview.student}
         interviewer={props.interview.interviewer.name}
+        onDelete={()=>{
+          transition(CONFIRM)}}
         />
         )}
       {mode === CREATE && 
@@ -55,7 +71,16 @@ export default function Appointment(props) {
       onCancel={(() => back())}
       onSave={save}
       />}
+      {mode === CONFIRM && <Confirm 
+      onCancel={()=>{
+        back();
+      }}
+      onConfirm={()=>{
+        deleteAction();
+      }}
+      message={"ARE YOU SURE YOU WANT TO DELETE?"}/>}
       {mode === SAVING && <Status message={"SAVING"}/>}
+      {mode === DELETE && <Status message={"DELETING"}/>}
     </article>
   )
 }
