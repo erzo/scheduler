d4c3542a0da1001f2fcdaf98f2eb3a77d6018bfd
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-08-07 21:46:40 -0700

    create the delete function

--- src/components/Application.js
+++ src/components/Application.js
@@ -114,6 +114,7 @@ export default function Application(props) {
       interview={setInterview}
       interviewers={setInterviewers}
       bookInterview={bookInterview}
+      cancelInterview={cancelInterview}
     />)
   })
 
@@ -135,6 +136,23 @@ export default function Application(props) {
 
     return axios.put(`/api/appointments/${id}`, appointment)
     
+  }
+
+  function cancelInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    }
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    setState({
+      ...state,
+      appointments
+    });
+
+    return axios.delete(`/api/appointments/${id}`);
   }
 
 
