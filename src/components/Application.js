0928118979e69bdcbe317c446922f56a2876d2f1
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-08-12 17:38:52 -0700

    refactored code

--- src/components/Application.js
+++ src/components/Application.js
@@ -9,70 +9,6 @@ import useApplicationData from "../hooks/useApplicationData";
 
 import "components/Application.scss";
 
-// const days = [
-//   {
-//     id: 1,
-//     name: "Monday",
-//     spots: 2,
-//   },
-//   {
-//     id: 2,
-//     name: "Tuesday",
-//     spots: 5,
-//   },
-//   {
-//     id: 3,
-//     name: "Wednesday",
-//     spots: 0,
-//   },
-// ];
-
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 1,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 3,
-//     time: "2pm",
-//     interview: {
-//       student: "Logan Wolverine",
-//       interviewer: {
-//         id: 2,
-//         name: "Tori Malcolm",
-//         avatar: "https://i.imgur.com/Nmx0Qxo.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 4,
-//     time: "3pm",
-//   },
-//   {
-//     id: 5,
-//     time: "4pm",
-//     interview: {
-//       student: "Steve Rogers",
-//       interviewer: {
-//         id: 3,
-//         name: "Mildred Nazir",
-//         avatar: "https://i.imgur.com/T2WwVfS.png",
-//       }
-//     }
-//   }
-// ];
 
 export default function Application(props) {
 
@@ -91,7 +27,6 @@ export default function Application(props) {
   const setAppointments = getAppointmentsForDay(state, state.day);
   const setInterviewers = getInterviewersForDay(state, state.day);
   
-  // console.log("im here: ", setAppointments);
 
   let schedule = setAppointments.map((appointment) => {
     const setInterview = getInterview(state, appointment.interview);
